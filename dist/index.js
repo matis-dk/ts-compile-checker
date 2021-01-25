bf98d155e5ee25f77529d62999b33827912dbc4a
Mathias Piletti <mathiasp50@gmail.com>
2021-01-25 14:51:18 +0100

    Improved glob search; error handling; logging

--- dist/index.js
+++ dist/index.js
@@ -36,12 +36,24 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
 exports.__esModule = true;
-var glob = require("glob");
 var core = require("@actions/core");
 var child = require("child_process");
+var fg = require("fast-glob");
+var fs = require("fs");
+var util_1 = require("util");
+var readFile = util_1.promisify(fs.readFile);
 var spawnSync = child.spawnSync;
-var log = console.log;
+var isCI = Boolean(process.env.CI);
+var argInstall = true;
+var tscBin = "node_modules/.bin/tsc";
 log("Started TS compile checker");
 (function start() {
     return __awaiter(this, void 0, void 0, function () {
@@ -51,7 +63,9 @@ log("Started TS compile checker");
                 case 0: return [4 /*yield*/, getProjects()];
                 case 1:
                     projects = _a.sent();
-                    compileErrors = runCompilationChecks(projects);
+                    return [4 /*yield*/, runCompilationChecks(projects.filter(function (p) { return p !== "api"; }))];
+                case 2:
+                    compileErrors = _a.sent();
                     logErrors(compileErrors);
                     return [2 /*return*/];
             }
@@ -59,65 +73,179 @@ log("Started TS compile checker");
     });
 })();
 function getProjects() {
-    log("🔍 Searching for projects with a tsconfig.json file");
-    return new Promise(function (res, _) {
-        glob("./**/tsconfig.json", {}, function (err, files) {
-            if (err) {
-                log(err);
-                throw new Error("Failed search for tsconfig.json files");
+    return __awaiter(this, void 0, void 0, function () {
+        var timeSearchStart, files, timeSearchEnd, projects, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    log("🔍 Searching for projects with a tsconfig.json file");
+                    timeSearchStart = process.hrtime.bigint();
+                    _a.label = 1;
+                case 1:
+                    _a.trys.push([1, 3, , 4]);
+                    return [4 /*yield*/, fg("**/tsconfig.json", {
+                            ignore: ["**/node_modules/**", "**/build/**", "**/dist/**"]
+                        })];
+                case 2:
+                    files = _a.sent();
+                    timeSearchEnd = process.hrtime.bigint();
+                    projects = files.map(function (path) {
+                        if (path === "tsconfig.json") {
+                            return ".";
+                        }
+                        else {
+                            return path.replace("/tsconfig.json", "");
+                        }
+                    });
+                    log("\n📝 Projects found");
+                    logDiffStartEnd("\n⏰ Search took", timeSearchStart, timeSearchEnd);
+                    console.table(projects.map(function (project) { return ({ path: project }); }));
+                    return [2 /*return*/, projects];
+                case 3:
+                    err_1 = _a.sent();
+                    log(err_1);
+                    throw new Error("Failed search for tsconfig.json files");
+                case 4: return [2 /*return*/];
             }
-            var projects = files
-                .filter(function (f) { return !(f.includes("node_modules") || f.includes("ts-compile-check")); })
-                .map(function (path) { return path.replace("/tsconfig.json", ""); });
-            log("📝 Projects found");
-            console.table(projects.map(function (project) { return ({ path: project }); }));
-            res(projects);
         });
     });
 }
 function runCompilationChecks(projectPaths) {
-    try {
-        var compileErrors_1 = [];
-        core.info("🛠️  Checking for typescript compilation errors");
-        projectPaths.forEach(function (projectPath) {
-            var _a, _b;
-            var tscArgs = ["--noEmit", "--pretty"];
-            var options = {
-                cwd: projectPath
-            };
-            spawnSync("yarn", ["install"], options);
-            var output = spawnSync("node_modules/.bin/tsc", tscArgs, options);
-            if (output.status !== 0) {
-                compileErrors_1.push({
-                    projectPath: projectPath,
-                    stdout: (_a = output.stdout) === null || _a === void 0 ? void 0 : _a.toString(),
-                    stderr: (_b = output.stderr) === null || _b === void 0 ? void 0 : _b.toString()
-                });
+    var projectPaths_1, projectPaths_1_1;
+    var e_1, _a;
+    var _b, _c;
+    return __awaiter(this, void 0, void 0, function () {
+        var compileErrors, projectPath, tscArgs, options, err_2, _1, output, e_1_1, err_3;
+        return __generator(this, function (_d) {
+            switch (_d.label) {
+                case 0:
+                    _d.trys.push([0, 21, , 22]);
+                    compileErrors = [];
+                    log("\n🛠️  Checking for typescript compilation errors");
+                    _d.label = 1;
+                case 1:
+                    _d.trys.push([1, 14, 15, 20]);
+                    projectPaths_1 = __asyncValues(projectPaths);
+                    _d.label = 2;
+                case 2: return [4 /*yield*/, projectPaths_1.next()];
+                case 3:
+                    if (!(projectPaths_1_1 = _d.sent(), !projectPaths_1_1.done)) return [3 /*break*/, 13];
+                    projectPath = projectPaths_1_1.value;
+                    tscArgs = ["--noEmit", "--pretty"];
+                    options = {
+                        cwd: projectPath
+                    };
+                    log("\n\uD83D\uDC49 Project [" + projectPath + "]");
+                    if (!argInstall) return [3 /*break*/, 8];
+                    _d.label = 4;
+                case 4:
+                    _d.trys.push([4, 6, , 7]);
+                    return [4 /*yield*/, readFile(projectPath + "/package.json", {
+                            encoding: "utf8"
+                        }).then(JSON.parse)];
+                case 5:
+                    _d.sent();
+                    return [3 /*break*/, 7];
+                case 6:
+                    err_2 = _d.sent();
+                    log("\u2757  Can't find/parse package.json. Skipping!", {
+                        level: "WARN"
+                    });
+                    return [3 /*break*/, 12];
+                case 7:
+                    log("• yarn install");
+                    spawnSync("yarn", ["install"], options);
+                    _d.label = 8;
+                case 8:
+                    _d.trys.push([8, 10, , 11]);
+                    return [4 /*yield*/, readFile(projectPath + "/" + tscBin)];
+                case 9:
+                    _d.sent();
+                    return [3 /*break*/, 11];
+                case 10:
+                    _1 = _d.sent();
+                    log("\u2757  Can't find 'tsc' in " + tscBin + ". Skipping!", { level: "WARN" });
+                    return [3 /*break*/, 12];
+                case 11:
+                    log("• tsc compilling");
+                    output = spawnSync(tscBin, tscArgs, options);
+                    /** ADDING COMPILE ERRORS */
+                    if (output.status !== 0) {
+                        compileErrors.push({
+                            projectPath: projectPath,
+                            stdout: (_b = output.stdout) === null || _b === void 0 ? void 0 : _b.toString(),
+                            stderr: (_c = output.stderr) === null || _c === void 0 ? void 0 : _c.toString(),
+                            output: output
+                        });
+                    }
+                    _d.label = 12;
+                case 12: return [3 /*break*/, 2];
+                case 13: return [3 /*break*/, 20];
+                case 14:
+                    e_1_1 = _d.sent();
+                    e_1 = { error: e_1_1 };
+                    return [3 /*break*/, 20];
+                case 15:
+                    _d.trys.push([15, , 18, 19]);
+                    if (!(projectPaths_1_1 && !projectPaths_1_1.done && (_a = projectPaths_1["return"]))) return [3 /*break*/, 17];
+                    return [4 /*yield*/, _a.call(projectPaths_1)];
+                case 16:
+                    _d.sent();
+                    _d.label = 17;
+                case 17: return [3 /*break*/, 19];
+                case 18:
+                    if (e_1) throw e_1.error;
+                    return [7 /*endfinally*/];
+                case 19: return [7 /*endfinally*/];
+                case 20: return [2 /*return*/, compileErrors];
+                case 21:
+                    err_3 = _d.sent();
+                    log("ERROR: Failed to run childProcess", { level: "ERROR" });
+                    throw new Error(err_3);
+                case 22: return [2 /*return*/];
             }
         });
-        return compileErrors_1;
-    }
-    catch (err) {
-        core.setFailed("ERROR: Failed to run childProcess");
-        throw new Error(err);
-    }
+    });
 }
+// Utilities ----------------------------------------
 function logErrors(compileErrors) {
     if (compileErrors.length) {
-        core.setFailed("\u274C  Failed to compile " + compileErrors.length + " project" + (compileErrors.length > 1 ? "s" : ""));
+        log("\n\u274C  Failed to compile " + compileErrors.length + " projects", {
+            level: "ERROR"
+        });
         compileErrors.forEach(function (c) {
             console.log("------------------------");
             if (c.stdout) {
-                log("Project '" + c.projectPath + "'");
-                core.setFailed(c.stdout);
+                log(c.stdout, { level: "ERROR" });
             }
             if (c.stderr) {
-                core.setFailed(c.stderr);
+                log(c.stderr, { level: "ERROR" });
             }
+            console.log(c);
         });
-        process.exit();
     }
     else {
-        log("✔️  Successfully compiled all projects");
+        log("\n✔️  Successfully compiled all projects");
+    }
+}
+function logDiffStartEnd(label, start, end) {
+    var NS_PER_MS = BigInt(1e6);
+    var diff = Math.round(Number((end - start) / NS_PER_MS));
+    log(label + " " + diff + " ms");
+}
+function log(msg, options) {
+    if (isCI) {
+        if ((options === null || options === void 0 ? void 0 : options.level) === "ERROR") {
+            core.setFailed(msg);
+        }
+        if ((options === null || options === void 0 ? void 0 : options.level) === "WARN") {
+            core.warning(msg);
+        }
+        if ((options === null || options === void 0 ? void 0 : options.level) === "INFO") {
+            core.info(msg);
+        }
+    }
+    else {
+        console.log(msg);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,167 @@
 #!/usr/bin/env node
-import * as glob from "glob";
 import * as core from "@actions/core";
 import * as child from "child_process";
+import * as fg from "fast-glob";
+import * as fs from "fs";
+import { promisify } from "util";
 
+const readFile = promisify(fs.readFile);
 const { spawnSync } = child;
-const log = console.log;
+
+const isCI = Boolean(process.env.CI);
+const argInstall = true;
+const tscBin = `node_modules/.bin/tsc`;
 
 type CompileError = {
   projectPath: string;
   stdout: string | null;
   stderr: string | null;
+  output: any;
 };
 
 log("Started TS compile checker");
 
 (async function start() {
   const projects = await getProjects();
-  const compileErrors = runCompilationChecks(projects);
+  const compileErrors = await runCompilationChecks(
+    projects.filter((p) => p !== "api")
+  );
   logErrors(compileErrors);
 })();
 
-function getProjects(): Promise<string[]> {
+async function getProjects(): Promise<string[]> {
   log("🔍 Searching for projects with a tsconfig.json file");
-  return new Promise((res, _) => {
-    const globOptions = {
-      ignore: ["**/node_modules/**"],
-    };
-    glob("**/tsconfig.json", globOptions, (err, files) => {
-      if (err) {
-        log(err);
-        throw new Error("Failed search for tsconfig.json files");
-      }
+  const timeSearchStart = process.hrtime.bigint();
 
-      const projects = files.map((path) => path.replace("/tsconfig.json", ""));
-
-      log("📝 Projects found");
-      console.table(projects.map((project) => ({ path: project })));
-
-      res(projects);
+  try {
+    const files = await fg("**/tsconfig.json", {
+      ignore: ["**/node_modules/**", "**/build/**", "**/dist/**"],
     });
-  });
+    const timeSearchEnd = process.hrtime.bigint();
+
+    const projects = files.map((path) => {
+      if (path === "tsconfig.json") {
+        return ".";
+      } else {
+        return path.replace("/tsconfig.json", "");
+      }
+    });
+
+    log("\n📝 Projects found");
+    logDiffStartEnd("\n⏰ Search took", timeSearchStart, timeSearchEnd);
+
+    console.table(projects.map((project) => ({ path: project })));
+
+    return projects;
+  } catch (err) {
+    log(err);
+    throw new Error("Failed search for tsconfig.json files");
+  }
 }
 
-function runCompilationChecks(projectPaths: string[]) {
+async function runCompilationChecks(projectPaths: string[]) {
   try {
     const compileErrors: CompileError[] = [];
-    core.info("🛠️  Checking for typescript compilation errors");
+    log("\n🛠️  Checking for typescript compilation errors");
 
-    projectPaths.forEach((projectPath) => {
+    for await (const projectPath of projectPaths) {
       const tscArgs = ["--noEmit", "--pretty"];
       const options = {
         cwd: projectPath,
       };
 
-      spawnSync("yarn", ["install"], options);
+      log(`\n👉 Project [${projectPath}]`);
 
-      const output = spawnSync("node_modules/.bin/tsc", tscArgs, options);
+      /** INSTALLATION */
+      if (argInstall) {
+        try {
+          await readFile(`${projectPath}/package.json`, {
+            encoding: "utf8",
+          }).then(JSON.parse);
+        } catch (err) {
+          log(`❗  Can't find/parse package.json. Skipping!`, {
+            level: "WARN",
+          });
+          continue;
+        }
+        log("• yarn install");
+        spawnSync("yarn", ["install"], options);
+      }
 
+      /** TS COMPILATION */
+      try {
+        await readFile(`${projectPath}/${tscBin}`);
+      } catch (_) {
+        log(`❗  Can't find 'tsc' in ${tscBin}. Skipping!`, { level: "WARN" });
+        continue;
+      }
+      log("• tsc compilling");
+      const output = spawnSync(tscBin, tscArgs, options);
+
+      /** ADDING COMPILE ERRORS */
       if (output.status !== 0) {
         compileErrors.push({
           projectPath,
           stdout: output.stdout?.toString(),
           stderr: output.stderr?.toString(),
+          output,
         });
       }
-    });
+    }
 
     return compileErrors;
   } catch (err) {
-    core.setFailed("ERROR: Failed to run childProcess");
+    log("ERROR: Failed to run childProcess", { level: "ERROR" });
     throw new Error(err);
   }
 }
 
+// Utilities ----------------------------------------
+
 function logErrors(compileErrors: CompileError[]) {
   if (compileErrors.length) {
-    core.setFailed(
-      `❌  Failed to compile ${compileErrors.length} project${
-        compileErrors.length > 1 ? `s` : ""
-      }`
-    );
-
+    log(`\n❌  Failed to compile ${compileErrors.length} projects`, {
+      level: "ERROR",
+    });
     compileErrors.forEach((c) => {
       console.log("------------------------");
       if (c.stdout) {
-        log(`Project '${c.projectPath}'`);
-        core.setFailed(c.stdout);
+        log(c.stdout, { level: "ERROR" });
       }
       if (c.stderr) {
-        core.setFailed(c.stderr);
+        log(c.stderr, { level: "ERROR" });
       }
+      console.log(c);
     });
-    process.exit();
   } else {
-    log("✔️  Successfully compiled all projects");
+    log("\n✔️  Successfully compiled all projects");
+  }
+}
+
+function logDiffStartEnd(label: string, start: bigint, end: bigint) {
+  const NS_PER_MS = BigInt(1e6);
+  const diff = Math.round(Number((end - start) / NS_PER_MS));
+  log(`${label} ${diff} ms`);
+}
+
+type Levels = "ERROR" | "WARN" | "INFO";
+type LogOptions = {
+  level: Levels;
+};
+function log(msg: string, options?: LogOptions) {
+  if (isCI) {
+    if (options?.level === "ERROR") {
+      core.setFailed(msg);
+    }
+
+    if (options?.level === "WARN") {
+      core.warning(msg);
+    }
+
+    if (options?.level === "INFO") {
+      core.info(msg);
+    }
+  } else {
+    console.log(msg);
   }
 }
